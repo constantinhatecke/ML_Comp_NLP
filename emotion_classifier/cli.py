70e44f76d6120230d8e642cebdb01221f6f2e37b
constantinhatecke <138979462+constantinhatecke@users.noreply.github.com>
2025-05-27 23:27:25 +0200

    Update cli.py

--- emotion_classifier/cli.py
+++ emotion_classifier/cli.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 from emotion_classifier.inference_engine import load_model_and_tokenizer, predict_label
 from emotion_classifier.utils import get_kaggle_id
 
@@ -10,9 +11,11 @@ def main():
 
     if args.kaggle:
         print(get_kaggle_id())
+        sys.stdout.flush()
     elif args.input:
         model, tokenizer, label_encoder = load_model_and_tokenizer()
         prediction = predict_label(args.input, model, tokenizer, label_encoder)
         print(prediction)
+        sys.stdout.flush()
     else:
         print("Please provide either --input or --kaggle.")
